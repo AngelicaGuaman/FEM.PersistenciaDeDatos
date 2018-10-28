2c114a902c9e0f6d648f1a3a68ff73b294db93f7
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-10-28 11:03:51 +0100

    add header

--- app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
@@ -22,8 +22,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-import static java.lang.Integer.parseInt;
-
 public class MainActivity extends AppCompatActivity {
 
     JuegoCelta mJuego;
